089d0f8f16d243aca233bef582329197d13af250
Umut Muslihoglu <umut.muslihoglu@nosurrender.studio>
2024-03-13 18:51:51 +0300

    main-TouchPhase.Moved-Seperator-methods

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
     private bool _isDragging;
     private BoardElement _previousElement;
     private BoardElement _selectedElement;
-    
+
 
     private void Update()
     {
@@ -41,61 +41,88 @@ public class PlayerController : MonoBehaviour
                 _isDragging = true;
                 lineRenderer.positionCount = 1;
                 lineRenderer.SetPosition(0, _selectedElement.transform.position);
-                // _selectedElement.SetCollider(false);
                 _selectedElements.Add(_selectedElement);
             }
             else if (_isDragging && touch.phase == TouchPhase.Moved)
             {
-                var elementAtTouch = GetElementAtTouchPosition(touch.position);
-                if (elementAtTouch == null) return;
-
-                if (Vector2.Distance(mainCamera.ScreenToWorldPoint(touch.position), _selectedElements[_selectedElements.Count - 1].transform.position) <= _selectionRange)
-                {
-                    if (elementAtTouch != _previousElement && elementAtTouch.GetNumber() == _selectedElement.GetNumber())
-                    {
-                        if (_selectedElements.Contains(elementAtTouch))
-                        {
-                            if (_selectedElements.Count == 1) return;
-                            if (elementAtTouch == _selectedElements[_selectedElements.Count -2])
-                            {
-                                if (_selectedElements.Count >= 2)
-                                {
-                                    Debug.LogError("Removing the last element");
-                                    lineRenderer.positionCount--;
-                                    lineRenderer.SetPosition(lineRenderer.positionCount - 1, elementAtTouch.transform.position);
-                                    _selectedElements[_selectedElements.Count - 1].Deselect();
-                                    _selectedElements.Remove(_selectedElements[^1]);
-                                }
-                                else
-                                {
-                                    Debug.LogError("Removing the last element and clearing the list");
-                                    _selectedElements.Clear();
-                                    lineRenderer.positionCount = 0;
-                                    _selectedElements[_selectedElements.Count - 1].Deselect();
-                                }
-                            }
-                            return;
-                        }
-                        
-                        lineRenderer.positionCount++;
-                        lineRenderer.SetPosition(lineRenderer.positionCount - 1, elementAtTouch.transform.position);
-                        _previousElement = elementAtTouch;
-                        // elementAtTouch.SetCollider(false);
-                        elementAtTouch.Select();
-                        _selectedElements.Add(elementAtTouch);
-                    }
-                }
+                HandleTouchMove(touch);
             }
             else if (_isDragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled))
             {
                 _isDragging = false;
-                // EnableColliders();
                 _selectedElement.Deselect(); // Deselect the first selected object
                 DeselectAllElements();
                 DestroyElements();
                 lineRenderer.positionCount = 0;
             }
         }
+    }
+
+    private void HandleTouchMove(Touch touch)
+    {
+        var elementAtTouch = GetElementAtTouchPosition(touch.position);
+        if (elementAtTouch == null) return;
+
+        if (IsWithinSelectionRange(touch, _selectedElements[_selectedElements.Count - 1].transform.position))
+        {
+            ProcessElementAtTouch(elementAtTouch);
+        }
+    }
+
+    private bool IsWithinSelectionRange(Touch touch, Vector3 position)
+    {
+        return Vector2.Distance(mainCamera.ScreenToWorldPoint(touch.position), position) <= _selectionRange;
+    }
+
+    private void ProcessElementAtTouch(BoardElement elementAtTouch)
+    {
+        if (elementAtTouch != _previousElement && elementAtTouch.GetNumber() == _selectedElement.GetNumber())
+        {
+            if (_selectedElements.Contains(elementAtTouch))
+            {
+                HandleElementAlreadySelected(elementAtTouch);
+                return;
+            }
+
+            AddElementToSelection(elementAtTouch);
+        }
+    }
+
+    private void HandleElementAlreadySelected(BoardElement elementAtTouch)
+    {
+        if (_selectedElements.Count == 1) return;
+        if (elementAtTouch == _selectedElements[_selectedElements.Count - 2])
+        {
+            RemoveLastElementFromSelection();
+        }
+    }
+
+    private void RemoveLastElementFromSelection()
+    {
+        if (_selectedElements.Count >= 2)
+        {
+            Debug.LogError("Removing the last element");
+            lineRenderer.positionCount--;
+            lineRenderer.SetPosition(lineRenderer.positionCount - 1, _selectedElements[^2].transform.position);
+            _selectedElements[_selectedElements.Count - 1].Deselect();
+            _selectedElements.Remove(_selectedElements[^1]);
+        }
+        else
+        {
+            Debug.LogError("Removing the last element and clearing the list");
+            _selectedElements.Clear();
+            lineRenderer.positionCount = 0;
+            _selectedElements[_selectedElements.Count - 1].Deselect();
+        }
+    }
+
+    private void AddElementToSelection(BoardElement elementAtTouch)
+    {
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount - 1, elementAtTouch.transform.position);
+        _previousElement = elementAtTouch;
+        elementAtTouch.Select();
+        _selectedElements.Add(elementAtTouch);
     }
 
     private void DeselectAllElements()
@@ -120,8 +147,6 @@ public class PlayerController : MonoBehaviour
         {
             element.SetCollider(true);
         }
-
-        // _selectedElements.Clear(); // Clear the list for the next use
     }
 
     private BoardElement GetElementAtTouchPosition(Vector2 touchPosition)
@@ -132,7 +157,6 @@ public class PlayerController : MonoBehaviour
         {
             return hit.collider.GetComponent<BoardElement>();
         }
-
         return null;
     }
 
@@ -158,15 +182,14 @@ public class PlayerController : MonoBehaviour
                 hit.collider.transform.DOMove(lastElement.transform.position, 0.20f).OnComplete(() =>
                 {
                     lastElement.SetNumber(totalNumber);
-                    
+
                     lastElement.Deselect(); // Deselect the last element scale it down
                     boardManager.SetCellToNull(position); // Set the cell to null before returning the object to the pool
                     ObjectPool.Instance.Return(hit.collider.gameObject);
                 });
             }
         }
-        
-        
+
         MoveDownAndFill();
         ResetTemporaryValues();
     }
@@ -174,6 +197,7 @@ public class PlayerController : MonoBehaviour
     private static int CalculateThreshold(List<BoardElement> selectedElements)
     {
         var total = 0;
+        // Calculate the total number of the selected elements
         foreach (var element in selectedElements)
         {
             total += element.GetNumber();
@@ -181,12 +205,12 @@ public class PlayerController : MonoBehaviour
 
         var thresholds = BoardManager.ElementValues;
         var newNumber = thresholds[0];
-
-        foreach (var t in thresholds)
+        // Find the threshold that is less than or equal to the total
+        foreach (var threshold in thresholds)
         {
-            if (total >= t)
+            if (total >= threshold)
             {
-                newNumber = t;
+                newNumber = threshold;
             }
             else
             {
